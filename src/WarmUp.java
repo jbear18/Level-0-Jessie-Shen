b1f9fd0bf5fa0d1f8e915cdf22f42d89e555a2a0
varnit05 <league@WTS10.attlocal.net>
2017-08-04 15:56:26 -0700

    Warm Up

---
+++ src/WarmUp.java
@@ -0,0 +1,15 @@
+
+public class WarmUp {
+public static void main(String[] args) {
+for (int i = 0; i < 100; i++) {
+if(i%2==0);
+	System.out.println("X");	
+if(i%3==0);
+	System.out.println("O");
+}
+}
+
+
+
+		}
+	
