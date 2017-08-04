07c4d3a2bc47d7cfcd497f69ce527b9288fd0e94
varnit05 <league@WTS10.attlocal.net>
2017-08-04 16:59:02 -0700

    BabyBabyBabyOh

---
+++ src/BabyBabyBabyOh.java
@@ -0,0 +1,14 @@
+
+public class BabyBabyBabyOh {
+public static void main(String[] args) {
+for (int i = 0; i < 10; i++) {
+if(i<4){
+System.out.println("baby");	
+}if(i>3){
+System.out.println("oh");
+}if(i%5==0){
+System.out.println("no");	
+}	
+}	
+}
+}
