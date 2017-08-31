adff9e8d4155cc80d4be3554e088251584c8fd19
100297 <league@WTS7.attlocal.net>
2017-08-30 20:02:29 -0700

    CrazyCatLady

---
+++ src/CrazyCatLady.java
@@ -0,0 +1,36 @@
+import java.net.URI;
+
+import javax.swing.JOptionPane;
+
+public class CrazyCatLady {
+	public static void main(String[] args) {
+		// 1. Ask the user how many cats they have
+		String ans = JOptionPane.showInputDialog("How many cats do you have?");
+		// 2. Convert their answer into an int
+		int ansNumber = Integer.parseInt(ans);
+		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
+		if (ansNumber > 3) {
+			JOptionPane.showMessageDialog(null, "You're a crazy cat lady!");
+		}
+
+		// 4. If they have 3 or less, call the method below to show them a cat video
+		if (ansNumber < 3) {
+		playVideo("https://www.youtube.com/watch?v=nG2rNBFzkGE");
+		}
+		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
+		if(ansNumber==0){
+		playVideo("https://www.youtube.com/watch?v=W_juM14WHNQ");	
+		}
+		}
+	
+
+	static void playVideo(String videoURL) {
+		try {
+			URI uri = new URI(videoURL);
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
