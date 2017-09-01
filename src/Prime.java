5a344ed3490f939fe343e428fe94831172a98ad6
jbear18 <league@WorthyMini-6.attlocal.net>
2017-09-01 16:55:33 -0700

    Prime

--- src/Prime.java
+++ src/Prime.java
@@ -1,14 +1,34 @@
 import javax.swing.JOptionPane;
 
 public class Prime {
-public static void main(String[] args) {
-String ans=	JOptionPane.showInputDialog("Insert a number below");
-int ansNumber= Integer.parseInt(ans);	
-	if(ansNumber<2) {
-JOptionPane.showMessageDialog(null, "Your number is not prime!");	
+	public static void main(String[] args) {
+		String ans = JOptionPane.showInputDialog("Insert a number below");
+		int ansNumber = Integer.parseInt(ans);
+		if (ansNumber < 2) {
+			JOptionPane.showMessageDialog(null, "Your number is not prime!");
+System.exit(0);
+		}
+		for (int i = 2; i < ansNumber; i++) {
+			if (ansNumber % 2 == 0) {
+				JOptionPane.showMessageDialog(null, "You're number is not prime!");
+				System.exit(0);
+			}
+			
+		}
+		for (int i = 2; i < ansNumber; i++) {
+			if(ansNumber%3==0) {
+				JOptionPane.showMessageDialog(null, "You're number is not prime");
+				System.exit(0);
+			}
+			}
+		JOptionPane.showMessageDialog(null, "You're number is PRIME!");
+		System.exit(0);
+		}
 	
 	
 	
-}
-}
-}
+		
+		
+	}
+
+
