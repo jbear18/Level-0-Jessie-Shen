6470831c5449e201cbee761fed9acf6b3491ec02
jbear18 <league@WorthyMini-6.attlocal.net>
2017-09-15 16:57:10 -0700

    Number Sorter

---
+++ src/NumberSorter.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class NumberSorter {
+public static void main(String[] args) {
+String num1= JOptionPane.showInputDialog("Enter your first number.");
+int int1=Integer.parseInt(num1);
+	String num2= JOptionPane.showInputDialog("Enter your second number.");
+	int int2=Integer.parseInt(num2);
+	String num3=JOptionPane.showInputDialog("Enter your third number.");
+	int int3=Integer.parseInt(num3);
+	
+	
+	
+	
+	
+	
+}
+}	
